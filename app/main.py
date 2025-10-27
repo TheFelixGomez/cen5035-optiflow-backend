876c3e1c62bbc428ed300b58943223f23da5f1d2
kdunn029 <kdunn0718@gmail.com>
2025-10-27 19:44:05 -0400

    Add CRUD routers for vendors and orders
    Added vendors_router.py and orders_router.py with full CRUD endpoints. Connected routers in main.py. Updated database.py and added serializer functions.

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
-
+from app.routers import vendors_router, orders_router
 from app.database import users_collection
 
 app = FastAPI()
 
+app.include_router(vendors_router.router)
+app.include_router(orders_router.router)
 
 @app.get("/ping")
 def ping():
-    return "pong"
+    return {"message":"pong"}
 
 
 @app.get("/db-ping")
 async def db_ping():
     await users_collection.find_one({})
-
-    return "db pong"
+    return {"message":"db pong"}
