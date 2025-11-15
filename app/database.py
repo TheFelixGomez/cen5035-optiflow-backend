038e018a318d8fdd56c0d1eda666dd3a1d303630
anaceciliaaa-ba <anaceciliabarahonaa@gmail.com>
2025-11-14 20:59:38 -0500

    Switch to AsyncMongoClient for MongoDB access
    Updated MongoDB client to use AsyncMongoClient and modified collection access syntax.

--- app/database.py
+++ app/database.py
@@ -1,20 +1,14 @@
 from decouple import config
-from pymongo import MongoClient
+from pymongo import AsyncMongoClient
 
-# Create MongoDB client
-client = MongoClient(config("MONGODB_URL").strip('"'))
+client = AsyncMongoClient(config("MONGODB_URL").strip('"'))
 
-# Access to database
-mongo_db = client["optiflow"]
+# Access to Optiflow MongoDB
+mongo_db = client.optiflow
 
-# Access to collections
-users_collection = mongo_db["users"]
-orders_collection = mongo_db["orders"]
-vendors_collection = mongo_db["vendors"]
-products_collection = mongo_db["products"]
-
-# Dependency for FastAPI: returns DB instance per request
-def get_db():
-    return mongo_db
-
+# Access to all collections on Optiflow MongoDB
+users_collection = mongo_db.users
+orders_collection = mongo_db.orders
+vendors_collection = mongo_db.vendors
+products_collection = mongo_db.products
 
